3e019f79afa599c3ff1eb19a68980d8f9443d7bd
“Joseph <joseph.d.jaafari@gmail.com>
2016-03-08 13:39:03 -0500

    adding new js and readme

--- torture.js
+++ torture.js
@@ -1,12 +1,22 @@
 var zip = parsFloat(zipCode);
-function populatehtml(operation){
+function populatehtml(locOperation){
    document.getElementById('zipCode')
    if endsWith('stringOne')
    // need fnct for returns w/ location user should go to based on string
-   var location = operation();
+   var location = locOperation();
 
 document.getElementById('closestLocation').innerText += location
 }
 
 document.getElementById('calc').onclick = function(){
-   populatehtml(operation)};
+   populatehtml(operation);
+
+ 
+ function populatehtml(waitOperation){
+   document.getElementById('zipCode')
+   if endsWith('stringOne')
+      // need fnct that retrns w/ wait time based on location
+   var wait= waitOperation();
+
+document.getElementById('waitTime').innerText += wait
+ }
