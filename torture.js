dda18eda31acc6204b99038278d869abd30e8834
“Joseph <joseph.d.jaafari@gmail.com>
2016-03-10 20:55:50 -0500

    everything in jesus' name

--- torture.js
+++ torture.js
@@ -1,23 +1,43 @@
-var zip = parsFloat(zipCode);
-function populatehtml(locOperation){
-   document.getElementById('zipCode')
-   if endsWith('stringOne')
-   // need fnct for returns w/ location user should go to based on string
-   var location = locOperation();
+/* All of the things
+ * Joseph Jaafari
+ * March 10, 2016
+ */
 
-document.getElementById('closestLocation').innerText += location
-}
 
-document.getElementById('calc').onclick = function(){
-   populatehtml(operation);
 
- 
- function populatehtml(waitOperation){
-   document.getElementById('zipCode')
-   if endsWith('stringOne')
-      // need fnct that retrns w/ wait time based on location
-   var wait= waitOperation();
 
-document.getElementById('waitTime').innerText += wait
- }
 
+
+var lookup = function(zip) {
+        //borrowed from http://docs.cartodb.com/cartodb-platform/cartodb-js/sql/
+        var sql = new cartodb.SQL({
+            user: 'josephjaafari'
+        });
+
+        sql.execute("SELECT a.display_name, a.wait_time, b.zip, a.lat as uscis_lat, a.lng as uscis_lng, (ST_Distance(ST_POINT(a.lng::float,a.lat::float)::geography, ST_POINT(b.lng::float, b.lat::float)::geography) / 1609.34) as distance FROM uscis_locations a JOIN uscis_data b ON a.id = b.uscis_id WHERE zip = {{zip}}", {
+            zip: zip
+        })
+            .done(function(data) {
+                var results = data.rows[0]
+                $("#closestLocation").html(results.display_name);
+                $("#waitTime").html(results.wait_time + " years");
+                $("#distance").html(parseInt(results.distance) + " miles");
+            })
+            .error(function(errors) {
+                // errors contains a list of errors
+                console.log("errors:" + errors);
+            });
+    },
+    map;
+
+
+$(document).ready(function() {
+
+    $("body").on("click", "#calc", function() {
+        var zip = parseFloat($("#zipCode").val());
+
+        lookup(zip);
+    });
+
+
+});
